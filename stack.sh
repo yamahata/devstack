1263ac3aa68e4f7176c39fca3c74934c4a8eda9d
Isaku Yamahata <yamahata@valinux.co.jp>
2012-09-24 22:22:44 +0900

    stack.sh: debug
    Signed-off-by: Isaku Yamahata <yamahata@valinux.co.jp>

--- stack.sh
+++ stack.sh
@@ -200,9 +200,9 @@ if [[ $EUID -eq 0 ]]; then
     cp -r -f -T "$PWD" "$STACK_DIR"
     chown -R stack "$STACK_DIR"
     if [[ "$SHELL_AFTER_RUN" != "no" ]]; then
-        exec su -c "set -e; cd $STACK_DIR; bash stack.sh; bash" stack
+        exec su -c "set -e; cd $STACK_DIR; bash -x stack.sh; bash" stack
     else
-        exec su -c "set -e; cd $STACK_DIR; bash stack.sh" stack
+        exec su -c "set -e; cd $STACK_DIR; bash -x stack.sh" stack
     fi
     exit 1
 else
@@ -1188,6 +1188,7 @@ fi
 
 if is_service_enabled quantum; then
     echo_summary "Configuring Quantum"
+    set -x
     #
     # Quantum Network Configuration
     #

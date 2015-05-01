291f277e645d76a8d3ad486f67edc71867817ea3
Isaku Yamahata <isaku.yamahata@intel.com>
2015-05-01 07:23:44 -0700

    exercise tacker vnf command

---
+++ exercises/tacker-vnf.sh
@@ -0,0 +1,141 @@
+#!/usr/bin/env bash
+
+# **neutron-servicevm.sh**
+
+# Test servicevm via the command line
+
+echo "*********************************************************************"
+echo "Begin DevStack Exercise: $0"
+echo "*********************************************************************"
+
+# This script exits on an error so that errors don't compound and you see
+# only the first error that occurred.
+set -o errexit
+
+# Print the commands being run so that we can see the command that triggers
+# an error.  It is also useful for following allowing as the install occurs.
+set -o xtrace
+
+
+# Settings
+# ========
+
+# Keep track of the current directory
+EXERCISE_DIR=$(cd $(dirname "$0") && pwd)
+TOP_DIR=$(cd $EXERCISE_DIR/..; pwd)
+
+# Import common functions
+source $TOP_DIR/functions
+
+# Import configuration
+source $TOP_DIR/openrc
+
+# Import exercise configuration
+source $TOP_DIR/exerciserc
+
+# Skip if the hypervisor is Docker
+[[ "$VIRT_DRIVER" == "docker" ]] && exit 55
+
+
+# Testing neutron servicevm
+# =========================
+
+# create necessary networks
+# prepare network
+NET_MGMT=net_mgmt
+SUBNET_MGMT=subnet_mgmt
+FIXED_RANGE_MGMT=10.253.255.0/24
+NETWORK_GATEWAY_MGMT=10.253.255.1
+
+NET0=net0
+SUBNET0=subnet0
+FIXED_RANGE0=10.253.0.0/24
+NETWORK_GATEWAY0=10.253.0.1
+
+NET1=net1
+SUBNET1=subnet1
+FIXED_RANGE1=10.253.1.0/24
+NETWORK_GATEWAY1=10.253.1.1
+
+for net in ${NET_MGMT} ${NET0} ${NET1}
+do
+    for i in $(neutron net-list | awk "/${net}/{print \$2}")
+    do
+	neutron net-delete $i
+    done
+done
+
+
+NET_MGMT_ID=$(neutron net-create ${NET_MGMT} | awk '/ id /{print $4}')
+SUBNET_MGMT_ID=$(neutron subnet-create --name ${SUBNET_MGMT} --ip-version 4 --gateway ${NETWORK_GATEWAY_MGMT} ${NET_MGMT_ID} ${FIXED_RANGE_MGMT} | awk '/ id /{print $4}')
+NET0_ID=$(neutron net-create ${NET0} | awk '/ id /{print $4}')
+SUBNET0_ID=$(neutron subnet-create --name ${SUBNET0} --ip-version 4 --gateway ${NETWORK_GATEWAY0} ${NET0_ID} ${FIXED_RANGE0} | awk '/ id /{print $4}')
+NET1_ID=$(neutron net-create ${NET1} | awk '/ id /{print $4}')
+SUBNET1_ID=$(neutron subnet-create --name ${SUBNET1} --ip-version 4 --gateway ${NETWORK_GATEWAY1} ${NET1_ID} ${FIXED_RANGE1} | awk '/ id /{print $4}')
+
+echo ${NET_MGMT_ID}
+echo ${SUBNET_MGMT_ID}
+echo ${NET0_ID}
+echo ${SUBNET0_ID}
+echo ${NET1_ID}
+echo ${SUBNET1_ID}
+
+VNFD_NAME=vnfd-demo
+#VNFD_FILE=/home/yamahata/openstack/tacker/tacker/vnfd-template/vnfd-use-sample2.yaml
+#VNFD_ID=$(tacker vnfd-create --name ${VNFD_NAME} --vnfd-file ${VNFD_FILE})
+VNFD_DATA=$(cat <<EOF
+template_name: sample-vnfd
+description: demo-example
+
+service_properties:
+  Id: sample-vnfd
+  vendor: tacker
+  version: 1
+
+vdus:
+  vdu1:
+    id: vdu1
+    vm_image: cirros-0.3.2-x86_64-uec
+    instance_type: m1.micro
+
+    network_interfaces:
+      management:
+        network: net_mgmt
+      pkt_in:
+        network: net0
+      pkt_out:
+        network: net1
+
+    placement_policy:
+      availability_zone: nova
+
+    auto-scaling: noop
+    monitoring_policy: ping
+    failure_policy: respawn
+
+    config:
+      param0: key0
+      param1: key1
+EOF
+)
+
+VNFD_ID=$(tacker vnfd-create --name ${VNFD_NAME} --vnfd "${VNFD_DATA}" | awk '/ id /{print $4}')
+
+
+#CONFIG_FILE=/home/yamahata/openstack/tacker/tacker/vnfd-template/config.yaml
+#VNF_ID=$(tacker vnf-create --vnfd-id ${VNFD_ID} --config-file ${CONFIG_FILE})
+CONFIG_DATA=$(cat <<'EOF'
+vdus:
+  vdu1:
+    config:
+      conf0: value0
+      conf1: value1
+EOF
+)
+
+neutVNF_ID=$(tacker vnf-create --vnfd-id ${VNFD_ID} --config "${CONFIG_DATA}" | awk '/ id /{print $4}')
+
+set +o xtrace
+echo "*********************************************************************"
+echo "SUCCESS: End DevStack Exercise: $0"
+echo "*********************************************************************"

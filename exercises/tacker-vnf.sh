c1a6a4dc7c2e5f35d32992f9e3ca4855b8a51223
Isaku Yamahata <isaku.yamahata@intel.com>
2015-05-12 19:34:18 -0700

    update

--- exercises/tacker-vnf.sh
+++ exercises/tacker-vnf.sh
@@ -40,6 +40,14 @@ source $TOP_DIR/exerciserc
 # Testing neutron servicevm
 # =========================
 
+# wget https://downloads.openwrt.org/barrier_breaker/14.07/x86/kvm_guest/openwrt-x86-kvm_guest-combined-ext4.img.gz
+# gunzip openwrt-x86-kvm_guest-combined-ext4.img.gz
+# glance image-create --name openwrt-x86 --is-public=True --container-format bare --disk-format raw --property hw_disk_bus=ide --file openwrt-x86-kvm_guest-combined-ext4.img
+
+# create openwrt-x86 image
+# http://hackstack.org/x/blog/2014/08/17/openwrt-images-for-openstack/
+glance image-create --name OpenWRT --is-public=True --container-format bare --disk-format raw --file /home/yamahata/openstack/tacker/openwrt/bin/x86/openwrt-x86-kvm_guest-combined-ext4.img
+
 # create necessary networks
 # prepare network
 MGMT_PHYS_NET=mgmtphysnet0
@@ -60,7 +68,13 @@ SUBNET1=subnet1
 FIXED_RANGE1=10.253.1.0/24
 NETWORK_GATEWAY1=10.253.1.1
 
-for net in ${NET_MGMT} ${NET0} ${NET1}
+NET2=net2
+SUBNET2=subnet2
+FIXED_RANGE2=10.253.2.0/24
+NETWORK_GATEWAY2=10.253.2.1
+
+
+for net in ${NET_MGMT} ${NET0} ${NET1} ${NET2}
 do
     for i in $(neutron net-list | awk "/${net}/{print \$2}")
     do
@@ -69,12 +83,14 @@ do
 done
 
 
-NET_MGMT_ID=$(neutron net-create --provider:network_type flat --provider:physical_network ${MGMT_PHYS_NET} ${NET_MGMT} | awk '/ id /{print $4}')
+NET_MGMT_ID=$(neutron net-create --provider:network_type flat --provider:physical_network ${MGMT_PHYS_NET} --shared ${NET_MGMT} | awk '/ id /{print $4}')
 SUBNET_MGMT_ID=$(neutron subnet-create --name ${SUBNET_MGMT} --ip-version 4 --gateway ${NETWORK_GATEWAY_MGMT} ${NET_MGMT_ID} ${FIXED_RANGE_MGMT} | awk '/ id /{print $4}')
-NET0_ID=$(neutron net-create ${NET0} | awk '/ id /{print $4}')
+NET0_ID=$(neutron net-create --shared ${NET0} | awk '/ id /{print $4}')
 SUBNET0_ID=$(neutron subnet-create --name ${SUBNET0} --ip-version 4 --gateway ${NETWORK_GATEWAY0} ${NET0_ID} ${FIXED_RANGE0} | awk '/ id /{print $4}')
-NET1_ID=$(neutron net-create ${NET1} | awk '/ id /{print $4}')
+NET1_ID=$(neutron net-create --shared ${NET1} | awk '/ id /{print $4}')
 SUBNET1_ID=$(neutron subnet-create --name ${SUBNET1} --ip-version 4 --gateway ${NETWORK_GATEWAY1} ${NET1_ID} ${FIXED_RANGE1} | awk '/ id /{print $4}')
+NET2_ID=$(neutron net-create --shared ${NET2} | awk '/ id /{print $4}')
+SUBNET2_ID=$(neutron subnet-create --name ${SUBNET2} --ip-version 4 --gateway ${NETWORK_GATEWAY2} ${NET2_ID} ${FIXED_RANGE2} | awk '/ id /{print $4}')
 
 echo ${NET_MGMT_ID}
 echo ${SUBNET_MGMT_ID}
@@ -82,6 +98,8 @@ echo ${NET0_ID}
 echo ${SUBNET0_ID}
 echo ${NET1_ID}
 echo ${SUBNET1_ID}
+echo ${NET2_ID}
+echo ${SUBNET2_ID}
 
 sudo ifconfig ${BR_MGMT} inet 0.0.0.0
 sudo ifconfig ${BR_MGMT} inet ${NETWORK_GATEWAY_MGMT_IP}
